0ab2bac32b5840f33b960166e987975e4a53c381
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-01-25 20:40:22 -0500

    added navbar content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import Navbar from "./components/navbar";
+import SavedPage from "./pages/saved";
+import SearchPage from "./pages/search";
+
 function App() {
-	return null;
+	const [view, setView] = useState("search");
+
+	// For three or more views to resolve
+	const renderView = () => {
+		if (view === "search") {
+			return <SearchPage />;
+		} else if (view === "saved") {
+			return <SavedPage />;
+		} else {
+			return "No matching view found";
+		}
+	};
+
+	return (
+		<>
+			<Navbar />
+			{renderView()}
+		</>
+	);
+
+	// Simple method to switch between only 2 views
+	// return (
+	// 	<>
+	// 		<Navbar
+	// 			view={view}
+	// 			setView={setView}
+	// 		/>
+	// 		{view === "search" ? <SearchPage /> : <SavedPage />}
+	// 	</>
+	// );
 }
 
 export default App;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,46 @@
-const navbar = (props) => {
-	return "navbar";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+const navbar = ({ view, setView }) => {
+	return (
+		<nav className="navbar navbar-expand-lg bg-danger">
+			<div className="container-fluid">
+				<a
+					className="navbar-brand text-light"
+					href="/"
+				>
+					Github Repo Search
+				</a>
+				<ul className="navbar-nav">
+					<li className="nav-item">
+						<a
+							onClick={() => {
+								setView("search");
+							}}
+							className={`nav-link text-light ${
+								view === "search" ? "active fw-bold" : ""
+							}`}
+							aria-current="page"
+							href="#"
+						>
+							<i className="bi bi-search-heart"></i> Search
+						</a>
+					</li>
+					<li className="nav-item">
+						<a
+							onClick={() => {
+								setView("saved");
+							}}
+							className={`nav-link text-light ${
+								view === "saved" ? "active fw-bold" : ""
+							}`}
+							href="#"
+						>
+							<i className="bi bi-bookmark-heart"></i> Saved
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default navbar;
