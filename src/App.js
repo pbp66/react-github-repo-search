912b06382454006713d4b883b06220db1e17760c
Josh <zylo.codes@gmail.com>
2023-01-25 19:40:25 -0600

    Navbar and routing between views

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import { useState } from 'react'
+import Navbar from './components/navbar'
+import SavedPage from './pages/saved'
+import SearchPage from './pages/search'
+
 function App() {
+  const [view, setView] = useState('search')
+
+  const renderView = () => {
+    if (view === 'search') {
+      return <SearchPage />
+    } else if (view === 'saved') {
+      return <SavedPage />
+    } else {
+      return 'No matching view found'
+    }
+  }
+
   return (
-    null
+    <>
+      <Navbar 
+        view={view}
+        setView={setView}
+      />
+      { renderView() }
+    </>
   );
 }
 
