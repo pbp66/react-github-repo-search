42265b6695ab86a110a9a786db4440f4c2467b0e
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-01-25 20:45:50 -0500

    found more files to merge

--- src/pages/saved.js
+++ src/pages/saved.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 const SavedPage = (props) => {
 	return "SavedPage";
 };
 
 export default SavedPage;
-=======
-const SavedPage = props => {
-  return 'SavedPage'
-}
-
-export default SavedPage
->>>>>>> 912b06382454006713d4b883b06220db1e17760c
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 const SearchPage = (props) => {
 	return "SearchPage";
 };
 
 export default SearchPage;
-=======
-const SearchPage = props => {
-  return 'SearchPage'
-}
-
-export default SearchPage
->>>>>>> 912b06382454006713d4b883b06220db1e17760c
