9ba8f5ee6cf73e8a53650449867c35e9cd5c9f9e
Josh <zylo.codes@gmail.com>
2023-01-25 20:35:48 -0600

    SearchForm component

--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -1,16 +1,23 @@
 import Button from './button'
 import Input from './input'
 
-const SearchForm = () => {
+const SearchForm = ({ term, setTerm, handleSubmit }) => {
   return (
-    <form>
-      <Input
-        name="search"
-        className="form-control bg-dark text-light"
-        placeholder="Search repos by keyword"
-        type="search"
-      />
-      <Button>Search</Button>
+    <form onSubmit={event => {
+      event.preventDefault()
+      handleSubmit()
+    }}>
+      <div className="input-group input-group-lg">
+        <Input
+          value={term}
+          onChange={event => setTerm(event.target.value)}
+          name="search"
+          className="form-control bg-dark text-light"
+          placeholder="Search repos by keyword"
+          type="search"
+        />
+        <Button disabled={!term}>Search</Button>
+      </div>
     </form>
   )
 }
--- src/pages/search.js
+++ src/pages/search.js
@@ -2,17 +2,25 @@ import { useState } from 'react'
 import Container from '../components/container'
 import SearchForm from '../components/searchForm'
 
-const SearchPage = props => {
+const SearchPage = () => {
   const [term, setTerm] = useState('React')
   const [loading, setLoading] = useState(false)
   const [repos, setRepos] = useState([])
   const [savedRepos, setSavedRepos] = useState([])
   
+  const searchRepos = () => {
+    console.log('Searching repos...', term)
+  }
+
   return (
     <Container className="mt-3">
       <h1>Search Github Repos: <span className="badge bg-secondary">{term}</span></h1>
 
-      <SearchForm />
+      <SearchForm 
+        term={term}
+        setTerm={setTerm}
+        handleSubmit={searchRepos}
+      />
 
       {/* List */}
 
